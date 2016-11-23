51c7f98ffdbc26101a7c8394309f85e01b1df86b
Sam Perry <u1265119@unimail.hud.ac.uk>
2016-11-23 20:40:42 +0000

    Finished code and plots for section 3

--- code/main.m
+++ code/main.m
@@ -15,94 +15,150 @@ function main()
     x = 1*sin(2*pi*128*(time/fs)) + 0.2*sin(2*pi*220*(time/fs)) + ...
     0.01*cos(2*pi*525*(time/fs));
 
-    % Compute the DFT of the sine wave
-    X = fft(x);
-    
-    % Pad with zeros to make vector 2048 samples long
-    X2 = fft([x, zeros(1, 2048-N)]);
-
-
-    % Calculate absolute magnitude of spectrum
-    X = abs(X);
-    X2 = abs(X2);
-
-    % Zero pad smaller vector so they are the same sie for plotting
-    % Kronecker padding technique taken from: http://stackoverflow.com/questions/11776251/inserting-variable-number-of-zeros-between-non-zero-elements-in-a-vector-in-matl
-    zero_p = length(X2) / length(X);
-    b = [1 zeros(1,zero_p-1)];
-    X = kron(X, b);
-    X(X == 0) = NaN;
-
-    N = 31;
-    tri_win = (((N-1.0)/2.0)-abs((0:N-1)-((N-1.0)/2.0)))*(2.0/(N-1.0));
-    % tri_win
+    % Generate triangle, rectangle, sine and hann windows based on formulas
+    % provided
     N = 32;
-    rect_win = ones(N);
+    tri_win = (((N-1.0)/2.0)-abs((0:N-1)-((N-1.0)/2.0)))*(2.0/(N-1.0));
+    rect_win = ones(1, N);
     sine_win = sin(pi*(0:N-1)/(N-1));
     hann_win = 0.5*(1-cos(2*pi*(0:N-1)/(N-1)));
+
     figure;
-    X = fft(sine_win, 1024);
-    X = fftshift(X);
+    plot_windowed_signal(x, rect_win, fs, 1);
+    title('Rectrangle')
+    plot_windowed_signal(x, tri_win, fs, 2);
+    title('Triangle')
+    plot_windowed_signal(x, sine_win, fs, 3);
+    title('Sine')
+    plot_windowed_signal(x, hann_win, fs, 4);
+    title('Hann')
+
+
+function plot_windowed_signal(sig, win, Fs, plot_ind)
+    sig = sig .* win;
+    
+    % Compute the DFT of the sine wave
+    X = fft(sig, 2048);
+
+    % Calculate normalized absolute magnitudes
     X = 20*log10(abs(X)/max(abs(X)));
-    max(X(437:460))
 
-    % [m, im] = min(X(459:467));
-    % %op1 = (im + 459) / 1024.;
-    % op1 = im + 459
-    % [m, im] = min(X(556:568));
-    % %op2 = (im + 556) /1024.;
-    % op2 = im + 556
-    % op2 - op1
+    % Calculate DFT indexes
+    n = 0:length(X)-1
+    % Get DFT size
+    N = length(n)
+    % Calculate bin frequencies
+    f = (n*Fs)/N
 
-    plot(X)
+    % Create subplot
+    subplot(2,2,plot_ind);
     hold on;
-    axis([int64((1024/2.)-(1024*0.2)), int64((1024/2.)+(1024*0.2)), -80, 5]);
-    X = fft(hann_win, 1024);
-    X = 20*log10(abs(X)/max(abs(X)));
-    X = fftshift(X);
-    max(X(418:445))
-    % [m, im] = min(X(446:457));
-    % %op1 = (im + 446) / 1024.;
-    % op1 = im + 446;
-    % [m, im] = min(X(574:584));
-    % %op2 = (im + 574) /1024.;
-    % op2 = im + 574;
-    % op2 - op1
-
-    plot(X, '--r')
-    %axis([-0.2, 0.2, -80, 5]);
-    set(gca,'XTickMode','manual');
-    set(gca,'XTick',int64([(1024/2.)-(1024*0.2), 512, (1024/2.)+(1024*0.2)]));
-    set(gca,'XtickLabels', [-0.2, 0, 0.2]);
-    legend('Sine','Hann')
-    return;
-
-    hold on;
-    plot(tri_win, 'b')
-    plot(hann_win, 'r')
-    plot(sine_win, 'g')
-    plot(rect_win, 'c')
-    axis([-1, 33, 0, 1.1])
-
-    figure
-
-    % Plot the absolute magnitude of the DFT to a graph
-    stem(X(1:2048/2));
-    hold on
-    plot(X2(1:2048/2));
-    % Set xlabel to show bin 10 bin indexes
-    set(gca,'XTickMode','manual');
-    set(gca,'XTick',int64((1:10)*(1024/10)));
-    set(gca,'XtickLabels',int64((1:10)*((fs/2)/10)));
-
-
-    % Turn on grid
+    % Plot DFT to the nyquist frequency
+    plot(n(1:(end/2)+1), X(1:(end/2)+1))
     grid on;
-    
-    % Add a title to the graph
-    title('DFT of x')
-    
-    % Label the x axis as 'Frequency'
-    xlabel('Bin Index')
-    % Label the y axis as 'Magnitude'
-    ylabel('Magnitude |X|')
+
+    % Set axis ticks
+    set(gca,'XTickMode','manual');
+    set(gca,'XTick',0:64:N-1);
+    set(gca,'XtickLabels', f(1:64:end));
+    axis([0, 1024, -80, 5])
+
+
+
+
+
+
+
+
+
+
+
+
+    % % Pad with zeros to make vector 2048 samples long
+    % X2 = fft([x, zeros(1, 2048-N)]);
+
+
+    % % Calculate absolute magnitude of spectrum
+    % X = abs(X);
+    % X2 = abs(X2);
+
+    % % Zero pad smaller vector so they are the same sie for plotting
+    % % Kronecker padding technique taken from: http://stackoverflow.com/questions/11776251/inserting-variable-number-of-zeros-between-non-zero-elements-in-a-vector-in-matl
+    % zero_p = length(X2) / length(X);
+    % b = [1 zeros(1,zero_p-1)];
+    % X = kron(X, b);
+    % X(X == 0) = NaN;
+
+    % N = 31;
+    % tri_win = (((N-1.0)/2.0)-abs((0:N-1)-((N-1.0)/2.0)))*(2.0/(N-1.0));
+    % % tri_win
+    % N = 32;
+    % rect_win = ones(N);
+    % sine_win = sin(pi*(0:N-1)/(N-1));
+    % hann_win = 0.5*(1-cos(2*pi*(0:N-1)/(N-1)));
+    % figure;
+    % X = fft(sine_win, 1024);
+    % X = fftshift(X);
+    % X = 20*log10(abs(X)/max(abs(X)));
+    % max(X(437:460))
+
+    % % [m, im] = min(X(459:467));
+    % % %op1 = (im + 459) / 1024.;
+    % % op1 = im + 459
+    % % [m, im] = min(X(556:568));
+    % % %op2 = (im + 556) /1024.;
+    % % op2 = im + 556
+    % % op2 - op1
+
+    % plot(X)
+    % hold on;
+    % axis([int64((1024/2.)-(1024*0.2)), int64((1024/2.)+(1024*0.2)), -80, 5]);
+    % X = fft(hann_win, 1024);
+    % X = 20*log10(abs(X)/max(abs(X)));
+    % X = fftshift(X);
+    % max(X(418:445))
+    % % [m, im] = min(X(446:457));
+    % % %op1 = (im + 446) / 1024.;
+    % % op1 = im + 446;
+    % % [m, im] = min(X(574:584));
+    % % %op2 = (im + 574) /1024.;
+    % % op2 = im + 574;
+    % % op2 - op1
+
+    % plot(X, '--r')
+    % %axis([-0.2, 0.2, -80, 5]);
+    % set(gca,'XTickMode','manual');
+    % set(gca,'XTick',int64([(1024/2.)-(1024*0.2), 512, (1024/2.)+(1024*0.2)]));
+    % set(gca,'XtickLabels', [-0.2, 0, 0.2]);
+    % legend('Sine','Hann')
+    % return;
+
+    % hold on;
+    % plot(tri_win, 'b')
+    % plot(hann_win, 'r')
+    % plot(sine_win, 'g')
+    % plot(rect_win, 'c')
+    % axis([-1, 33, 0, 1.1])
+
+    % figure
+
+    % % Plot the absolute magnitude of the DFT to a graph
+    % stem(X(1:2048/2));
+    % hold on
+    % plot(X2(1:2048/2));
+    % % Set xlabel to show bin 10 bin indexes
+    % set(gca,'XTickMode','manual');
+    % set(gca,'XTick',int64((1:10)*(1024/10)));
+    % set(gca,'XtickLabels',int64((1:10)*((fs/2)/10)));
+
+
+    % % Turn on grid
+    % grid on;
+    % 
+    % % Add a title to the graph
+    % title('DFT of x')
+    % 
+    % % Label the x axis as 'Frequency'
+    % xlabel('Bin Index')
+    % % Label the y axis as 'Magnitude'
+    % ylabel('Magnitude |X|')

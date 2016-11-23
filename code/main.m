6cd78a35caf0784e1a558d880390012f1aefb534
Sam Perry <u1265119@unimail.hud.ac.uk>
2016-11-23 09:22:06 +0000

    Finished plotting DFT of windows

--- code/main.m
+++ code/main.m
@@ -28,20 +28,58 @@ function main()
 
     % Zero pad smaller vector so they are the same sie for plotting
     % Kronecker padding technique taken from: http://stackoverflow.com/questions/11776251/inserting-variable-number-of-zeros-between-non-zero-elements-in-a-vector-in-matl
-    zero_p = length(X2) / length(X)
+    zero_p = length(X2) / length(X);
     b = [1 zeros(1,zero_p-1)];
-    X = kron(X, b)
-    X(X == 0) = NaN
+    X = kron(X, b);
+    X(X == 0) = NaN;
 
-    N = 31
-    tri_win = (((N-1.0)/2.0)-abs((0:N-1)-((N-1.0)/2.0)))*(2.0/(N-1.0))
+    N = 31;
+    tri_win = (((N-1.0)/2.0)-abs((0:N-1)-((N-1.0)/2.0)))*(2.0/(N-1.0));
     % tri_win
-    % N = 32
-    % rect_win = 
-    % sine_win = 
-    % hann_win = 
-    plot(tri_win)
+    N = 32;
+    rect_win = ones(N);
+    sine_win = sin(pi*(0:N-1)/(N-1));
+    hann_win = 0.5*(1-cos(2*pi*(0:N-1)/(N-1)));
+    figure;
+    X = fft(tri_win, 1024);
+    X = abs(X);
+    X = fftshift(X);
+    subplot(2,2,1)
+    plot(X)
+    title('Triangle')
+    axis([0, 1024, 0, inf]);
+
+    X = fft(rect_win, 1024);
+    X = abs(X);
+    X = fftshift(X);
+    subplot(2,2,2)
+    plot(X)
+    title('Rectangle')
+    axis([0, 1024, 0, inf]);
+
+    X = fft(sine_win, 1024);
+    X = abs(X);
+    X = fftshift(X);
+    subplot(2,2,3)
+    plot(X)
+    title('Sine')
+    axis([0, 1024, 0, inf]);
+
+    X = fft(hann_win, 1024);
+    X = abs(X);
+    subplot(2,2,4)
+    X = fftshift(X);
+    plot(X)
+    title('Hann')
+    axis([0, 1024, 0, inf]);
     return;
+
+    hold on;
+    plot(tri_win, 'b')
+    plot(hann_win, 'r')
+    plot(sine_win, 'g')
+    plot(rect_win, 'c')
+    axis([-1, 33, 0, 1.1])
 
     figure
 
